ed565118a06fa9f689cf31f1604c4b547137c2de
Minh Nguyen <minh26@unc.edu>
2023-09-14 16:23:41 -0400

    part 1 of EX01, finished LS04

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,9 @@
+"""EX01 - Chardle - A cute step toward Wordle."""
+
+__author__ = "730574011"
+
+five_char_word : str = input("Enter a 5-character word: ")
+single_char : str = input("Enter a single character: ")
+print("Searching for " + single_char + " in " + five_char_word)
+
+

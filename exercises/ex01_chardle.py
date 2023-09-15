086bee17eb1de2ab57c84bb729e28d36f3de5073
Minh Nguyen <minh26@unc.edu>
2023-09-15 02:07:51 -0400

    finished part 3 of ex01

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -6,4 +6,51 @@ five_char_word : str = input("Enter a 5-character word: ")
 single_char : str = input("Enter a single character: ")
 print("Searching for " + single_char + " in " + five_char_word)
 
+num_of_matches : int = 0
+
+if str(five_char_word[0]) == str(single_char): 
+    num_of_matches = num_of_matches + 1
+    print (single_char + " found at index 0" )
+
+if str(five_char_word[1]) == str(single_char): 
+    num_of_matches = num_of_matches + 1
+    print (single_char + " found at index 1" )
+
+if str(five_char_word[2]) == str(single_char): 
+    num_of_matches = num_of_matches + 1
+    print (single_char + " found at index 2" )
+
+if str(five_char_word[3]) == str(single_char): 
+   num_of_matches = num_of_matches + 1
+   print (single_char + " found at index 3" )
+
+if str(five_char_word[4]) == str(single_char): 
+    num_of_matches = num_of_matches + 1
+    print (single_char + " found at index 4" )
+
+if num_of_matches == 0:
+    print("No instances of " + single_char + " found in " + five_char_word)
+
+if num_of_matches == 1:
+    print(str(num_of_matches) +" instance of " + single_char + " found in " + five_char_word)
+
+if num_of_matches > 1:
+    print(str(num_of_matches) +" instances of " + single_char + " found in " + five_char_word)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 

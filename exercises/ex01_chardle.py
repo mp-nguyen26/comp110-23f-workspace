44e4bb1cd8f85c3aa22da361474f707760bb4eb7
Minh Nguyen <minh26@unc.edu>
2023-09-15 20:10:17 -0400

    finished all of ex01!!

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -3,7 +3,17 @@
 __author__ = "730574011"
 
 five_char_word : str = input("Enter a 5-character word: ")
+
+if len(five_char_word) != 5:
+    print("Error: Word must contain 5 characters")
+    exit()
+
 single_char : str = input("Enter a single character: ")
+
+if len(single_char) != 1:
+    print("Error: Character must be a single character")
+    exit()
+
 print("Searching for " + single_char + " in " + five_char_word)
 
 num_of_matches : int = 0

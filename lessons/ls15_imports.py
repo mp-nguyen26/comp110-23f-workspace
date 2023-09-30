ea70598216ea64620e30f17b4ed3c5ea9dbb7a4d
Minh Nguyen <minh26@unc.edu>
2023-09-29 22:29:00 -0400

    ls15, cq03

---
+++ lessons/ls15_imports.py
@@ -0,0 +1,7 @@
+"""Practice importong from other modules"""
+
+from lessons import ls15_my_functions
+
+print(ls15_my_functions.addition(1,2))
+
+print(ls15_my_functions.my_variable)
---
+++ lessons/random.py
@@ -0,0 +1,8 @@
+"""Use this for random stuffs"""
+
+def f(x: int) -> int:
+    return x
+    x *= 2
+    print(x)
+
+y: int = f(3)

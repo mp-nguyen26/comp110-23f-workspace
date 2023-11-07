3a3c51cfd9fc0da75b20e089f7a7a516045ac065
Minh Nguyen <minh26@unc.edu>
2023-11-06 23:28:05 -0500

    submit 3 cq07

--- lessons/CQ07/point.py
+++ lessons/CQ07/point.py
@@ -2,24 +2,22 @@
 
 from __future__ import annotations
 
+
 class Point:
     """Defining attributes of class Point."""
 
     x: float
     y: float
 
-
     def __init__(self, x_init: float, y_init: float):
         """Constructor."""
         self.x = x_init
         self.y = y_init
     
-
     def scale_by(self, factor: int):
         """Modifies point by multiplication with a factor."""
         self.x *= factor
         self.y *= factor
-
 
     def scale(self, factor: int) -> Point:
         """Creates new point by multiplication with a factor."""

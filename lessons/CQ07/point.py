c1163e6a8c50cd8f8aaaffffb3e2961f6a917fbc
Minh Nguyen <minh26@unc.edu>
2023-11-13 23:40:33 -0500

    cq08 submit 1

--- lessons/CQ07/point.py
+++ lessons/CQ07/point.py
@@ -9,7 +9,7 @@ class Point:
     x: float
     y: float
 
-    def __init__(self, x_init: float, y_init: float):
+    def __init__(self, x_init: float = 0.0, y_init: float = 0.0):
         """Constructor."""
         self.x = x_init
         self.y = y_init
@@ -22,4 +22,22 @@ class Point:
     def scale(self, factor: int) -> Point:
         """Creates new point by multiplication with a factor."""
         new_point: Point = Point(self.x * factor, self.y * factor)
+        return new_point
+    
+
+    def __str__(self) -> str:
+        """Makes printed point readable."""
+        output: str = f"x: {self.x}; y: {self.y}"
+        return output
+    
+
+    def __mul__(self, factor: int |float):
+        """Another way to implement scale using mult operator overload."""
+        new_point: Point = Point(self.x * factor, self.y * factor)
+        return new_point
+    
+
+    def __add__(self, factor: int | float):
+        """Addition operator overload."""
+        new_point: Point = Point(self.x + factor, self.y + factor)
         return new_point
